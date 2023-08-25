9b57581eda372ee5c440289c81971e26cbe36e35
xiangfuli <xiangfuli96@gmail.com>
2023-08-25 02:53:48 +0000

    use circle trajectory and difftune offcial dubincar controller

--- examples/module/controller_parameters_tuner/dubincar_controller_tuner.py
+++ examples/module/controller_parameters_tuner/dubincar_controller_tuner.py
@@ -7,47 +7,27 @@ from examples.module.dynamics.dubincar import DubinCar
 from pypose.module.controller_parameters_tuner import ControllerParametersTuner
 
 
-def get_shortest_path_between_angles(original_ori, des_ori):
-  e_ori = des_ori - original_ori
-  if abs(e_ori) > torch.pi:
-    if des_ori > original_ori:
-      e_ori = - (original_ori + 2 * torch.pi - des_ori)
-    else:
-      e_ori = des_ori + 2 * torch.pi - original_ori
-  return e_ori
-
-def get_ref_states(waypoints, dt, device):
+def get_ref_states(time, dt, device):
+    """
+    Generate the trajectory based on constant angular velocity and constant linear velocity
+    """
     car_desired_states = []
-    last_desired_state = torch.zeros(9, device=device)
 
-    for index, waypoint in enumerate(waypoints[1:]):
-        # get path attributes from the last desired state
-        last_waypoint = waypoints[index]
-        vel = (waypoint - last_waypoint) / dt
-        last_vel = torch.tensor([last_desired_state[2], last_desired_state[3]])
-        acc = (vel[0:2] - last_vel) / dt
-        last_desired_orientation, \
-        last_desired_orientation_dot, \
-        last_desired_orientation_ddot = last_desired_state[-3:]
-        current_orientation = torch.atan2(vel[1], vel[0])
-        last_desired_orientation_remaindar = \
-          torch.atan2(torch.sin(last_desired_orientation), torch.cos(last_desired_orientation))
-        delta_angle = get_shortest_path_between_angles(last_desired_orientation_remaindar, current_orientation)
-        current_orientation = last_desired_orientation + delta_angle
-        current_ori_dot = (current_orientation - last_desired_orientation) / dt
+    waypoint = torch.zeros(2, device=device)
+    orientation = torch.zeros(1, device=device)
+    angular_vel = torch.ones(1, device=device)
+    desired_vel = torch.ones(1, device=device)
+    zero_tensor = torch.zeros(1, device=device)
+
+    # get the waypoints and orientation at each timestamp using euler intergration
+    for index in range(1, int(time / dt)):
+        vel = desired_vel * torch.tensor([torch.cos(orientation), torch.sin(orientation)], device=device)
+        waypoint += vel * dt
+        orientation += angular_vel * dt
 
         car_desired_states.append(torch.tensor(
-          [
-            waypoint[0], \
-            waypoint[1], \
-            vel[0], \
-            vel[1], \
-            acc[0], \
-            acc[1], \
-            current_orientation, \
-            current_ori_dot, \
-            (current_ori_dot - last_desired_orientation_dot) / dt], device=device))
-        last_desired_state = car_desired_states[-1:][0]
+          [waypoint[0], waypoint[1], desired_vel, zero_tensor, \
+            orientation, angular_vel, zero_tensor], device=device))
     return car_desired_states
 
 def run(dynamic_system, controller, controller_parameters, initial_state, ref_states, dt):
@@ -84,17 +64,16 @@ def compute_loss(dynamic_system, controller, controller_parameters, penalty_coef
     return loss / len(ref_states)
 
 def func_to_get_state_error(state, ref_state):
-    ref_position, ref_velocity, ref_acceleration, ref_pose, ref_angular_vel, ref_angular_acc = \
-        ref_state[0:2], ref_state[2:4], ref_state[4:6], \
-        ref_state[6], ref_state[7], ref_state[8]
+    x_desired, y_desired, v_desired, acc_desired, \
+      ori_desired, w_desired, wdot_desired = ref_state
 
     return state - torch.stack(
        [
-          ref_position[0],
-          ref_position[1],
-          ref_pose,
-          torch.norm(ref_velocity[0]),
-          ref_angular_vel
+          x_desired,
+          y_desired,
+          ori_desired,
+          v_desired,
+          w_desired
        ])
 
 def get_sub_states(input_states, sub_state_index):
@@ -104,11 +83,12 @@ def get_sub_states(input_states, sub_state_index):
     return sub_states
 
 def subPlot(ax, x, y, y_ref, xlabel=None, ylabel=None):
-    ax.plot(x, y, label='result')
-    ax.plot(x, y_ref, label='reference')
+    res = []
+    for i in range(0, len(y)):
+        res.append(y_ref[i] - y[i])
+    ax.plot(x, res, label=ylabel)
     ax.legend()
     ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
 
 if __name__ == "__main__":
 
@@ -124,26 +104,18 @@ if __name__ == "__main__":
 
     # program parameters
     time_interval = 0.02
-    learning_rate = 0.005
+    learning_rate = 10
     # states tensor: x position, y position, orientation, velocity, angular_velocity
     initial_state = torch.zeros(5, device=args.device).double()
     # controller parameters: kp_position, kp_velocity, kp_orientation, kp_angular_velocity
-    initial_controller_parameters = torch.ones(4, device=args.device).double()
+    initial_controller_parameters = torch.ones(4, device=args.device).double() * 5
 
-    points = torch.tensor([[[0., 0., 0.],
-                            [1., 0, 0],
-                            [1., 1, 0],
-                            [2., 0, 0],
-                            [3., 1, 0],
-                            [4., 0, 0]]])
-
-    waypoints = pp.chspline(points, time_interval)[0]
-    ref_states = get_ref_states(waypoints, time_interval, args.device)
+    ref_states = get_ref_states(10, time_interval, args.device)
 
     dubincar = DubinCar()
 
     # start to tune the controller parameters
-    penalty_coefficient = 0.0001
+    penalty_coefficient = 0.0
     tuner = ControllerParametersTuner(learning_rate=learning_rate,
                                       penalty_coefficient=penalty_coefficient,
                                       device=args.device)
@@ -169,8 +141,8 @@ if __name__ == "__main__":
           ref_states,
           controller,
           controller_parameters,
-          (0.01 * torch.ones_like(controller_parameters),
-            10 * torch.ones_like(controller_parameters)),
+          (0.001 * torch.ones_like(controller_parameters),
+            20 * torch.ones_like(controller_parameters)),
           time_interval,
           states_to_tune,
           func_to_get_state_error
@@ -181,7 +153,7 @@ if __name__ == "__main__":
                             initial_state, ref_states, time_interval)
         print("Loss: ", loss)
 
-        if (last_loss_after_tuning - loss) < 0.0001:
+        if (last_loss_after_tuning - loss) < 0.00001:
             meet_termination_condition = True
             print("Meet tuning termination condition, terminated.")
         else:
@@ -189,17 +161,23 @@ if __name__ == "__main__":
 
     # plot the result
     # get the result with original and tuned controller parameters
-    original_system_states = run(dubincar, controller, torch.clone(initial_controller_parameters)
-                                  , initial_state, ref_states, time_interval)
+    original_system_states = run(dubincar, controller, torch.clone(initial_controller_parameters),
+                                 initial_state, ref_states, time_interval)
     new_system_states = run(dubincar, controller, controller_parameters, initial_state,
-        ref_states, time_interval)
+                                  ref_states, time_interval)
     ref_states.insert(0, torch.zeros(9, device=args.device))
-    time = torch.arange(0, len(points[0]) - 1 + time_interval, time_interval).numpy()
+    time = torch.arange(0, time_interval * len(ref_states), time_interval).numpy()
     f, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
-    subPlot(ax[0], time, get_sub_states(original_system_states, 0), get_sub_states(ref_states, 0), ylabel='X position')
-    subPlot(ax[0], time, get_sub_states(original_system_states, 1), get_sub_states(ref_states, 1), ylabel='Y position')
-    subPlot(ax[1], time, get_sub_states(new_system_states, 0), get_sub_states(ref_states, 0), ylabel='X after tuning')
-    subPlot(ax[1], time, get_sub_states(new_system_states, 1), get_sub_states(ref_states, 1), ylabel='Y after tuning')
+    ax[0].set_title("Tracking result before tuning")
+    ax[0].set_xlabel("time(s)")
+    ax[0].set_ylabel("Tracking error(m)")
+    ax[1].set_title("Tracking result after tuning")
+    ax[1].set_xlabel("time(s)")
+    ax[1].set_ylabel("Tracking error(m)")
+    subPlot(ax[0], time, get_sub_states(original_system_states, 0), get_sub_states(ref_states, 0), ylabel='X')
+    subPlot(ax[0], time, get_sub_states(original_system_states, 1), get_sub_states(ref_states, 1), ylabel='Y')
+    subPlot(ax[1], time, get_sub_states(new_system_states, 0), get_sub_states(ref_states, 0), ylabel='X')
+    subPlot(ax[1], time, get_sub_states(new_system_states, 1), get_sub_states(ref_states, 1), ylabel='Y')
     figure = os.path.join(args.save + 'dubincar_controller_tuner.png')
     plt.savefig(figure)
     print("Saved to", figure)
--- pypose/module/dubincar_controller.py
+++ pypose/module/dubincar_controller.py
@@ -6,23 +6,25 @@ class DubinCarController(PID):
         super(DubinCarController, self).__init__()
 
     def get_control(self, parameters, state, ref_state, feed_forward_quantity):
-        x_desired, y_desired, vx_desired, vy_desired, accx_desired, \
-          accy_desired, angle_desired, angle_dot_desired, angle_ddot_desired = ref_state
+        x_desired, y_desired, v_desired, acc_desired, \
+          ori_desired, w_desired, wdot_desired = ref_state
         px, py, orientation, vel, w = state
         kp, kv, kori, kw = parameters
 
-        orientation_cos = torch.cos(orientation)
-        orientation_sin = torch.sin(orientation)
+        ori_cos = torch.cos(orientation)
+        ori_sin = torch.sin(orientation)
+        des_ori_cos = torch.cos(ori_desired)
+        des_ori_sin = torch.sin(ori_desired)
 
         # acceleration output
         acceleration = \
-          kp * (orientation_cos * (x_desired - px) + orientation_sin * (y_desired - py)) \
-          + kv * (orientation_cos * (vx_desired - vel * orientation_cos)
-                  + orientation_sin * (vy_desired - vel * orientation_sin)) \
-          + accx_desired * orientation_cos + accy_desired * orientation_sin
+          kp * (ori_cos * (x_desired - px) + ori_sin * (y_desired - py)) \
+          + kv * (ori_cos * (v_desired * des_ori_cos - vel * ori_cos)
+            + ori_sin * (v_desired * des_ori_sin - vel * ori_sin)) \
+          + ori_cos * (acc_desired * des_ori_cos - v_desired * w_desired * des_ori_sin) \
+            + ori_sin * (acc_desired * des_ori_sin + v_desired * w_desired * des_ori_cos)
 
-        err_angle = angle_desired - orientation
-        orientation_ddot = kori * err_angle + \
-          kw * (angle_dot_desired - w) + angle_ddot_desired
+        err_angle = ori_desired - orientation
+        orientation_ddot = kori * err_angle + kw * (w_desired - w) + wdot_desired
 
         return torch.stack([acceleration, orientation_ddot])
